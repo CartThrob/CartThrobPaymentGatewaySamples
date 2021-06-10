952e15bd12cd1e4d00cdd43d898cf9f6c3e76b7d
Eric Lamb <eric@mithra62.com>
2021-06-09 20:51:16 -0700

    adds basic add-on setup file details

--- system/user/addons/cartthrob_sample_gateway/addon.setup.php
+++ system/user/addons/cartthrob_sample_gateway/addon.setup.php
@@ -1,1 +1,15 @@
 <?php
+define('CARTTHROB_SAMPLE_GATEWAY_NAME', 'CartThrob Sample Payment Gateway');
+define('CARTTHROB_SAMPLE_GATEWAY_VERSION', '0.0.1');
+define('CARTTHROB_SAMPLE_GATEWAY_DESC', 'An example implementation of a Payment Gateway for CartThrob');
+
+return [
+    'author' => 'Foster Made',
+    'author_url' => 'https://cartthrob.com',
+    'docs_url' => '',
+    'name' => CARTTHROB_SAMPLE_GATEWAY_NAME,
+    'description' => CARTTHROB_SAMPLE_GATEWAY_DESC,
+    'version' => CARTTHROB_SAMPLE_GATEWAY_VERSION,
+    'namespace' => 'CartThrob\SampleGateway',
+    'settings_exist' => false,
+];
