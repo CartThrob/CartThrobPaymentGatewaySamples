a8da157b7ed3e004ced6f2bd93befffa63cb9917
Eric Lamb <eric@mithra62.com>
2021-06-11 21:28:10 -0700

    compiles basic language values

--- system/user/addons/cartthrob_sample_gateway/language/english/cartthrob_sample_gateway_lang.php
+++ system/user/addons/cartthrob_sample_gateway/language/english/cartthrob_sample_gateway_lang.php
@@ -1,1 +1,16 @@
 <?php
+
+$lang = [
+    'ct.payments.sample_gateway.title' => 'Sample Payment Gateway',
+    'ct.payments.sample_gateway.overview' => <<<EOF
+    This is just an example of how a Payment Gateway should be put together, so this blurb of text probably doesb't matter. 
+EOF
+    ,
+    'ct.payments.sample_gateway.api.public_key' => 'API Public Key',
+    'ct.payments.sample_gateway.api.public_key.note' => 'This would be a good spot to put a link to get said Public Key <a href="https://www.google.com/" target="_blank">place-to-get.com</a>',
+    'ct.payments.sample_gateway.api.private_key' => 'API Private Key',
+    'ct.payments.sample_gateway.api.sandbox.public_key' => 'Sandbox API Public Key',
+    'ct.payments.sample_gateway.api.sandbox.public_key.note' => 'If the Sandbox details are located elsewhere, here\'s a good spot for it. <a href="https://www.google.com/" target="_blank">place-to-get.com</a>',
+    'ct.payments.sample_gateway.api.sandbox.private_key' => 'Sandbox API Private Key',
+    'ct.payments.sample_gateway.api.mode' => 'Sandbox Mode?',
+];
